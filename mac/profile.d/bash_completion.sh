b57e52c69d70dea32d215605ac59e02b5681dbc7
Maxim Fedorenko <m.fedorenko@corp.mail.ru>
2017-09-14 14:24:33 +0300

    [mac] Update mac configs

--- mac/profile.d/bash_completion.sh
+++ mac/profile.d/bash_completion.sh
@@ -4,12 +4,10 @@ if [ -n "$BASH_VERSION" -a -n "$PS1" -a -z "$BASH_COMPLETION_COMPAT_DIR" ]; then
 # Check for recent enough version of bash.
 bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
 if [ $bmajor -gt 4 ] || [ $bmajor -eq 4 -a $bminor -ge 1 ]; then
-    [ -r "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion" ] && \
-        . "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion"
-    if shopt -q progcomp && [ -r /opt/local/share/bash-completion/bash_completion ]; then
-        # Source completion code.
-        . /opt/local/share/bash-completion/bash_completion
-    fi
+    for s in /usr/local/etc/bash_completion.d/* ; do
+        test -r $s -a ! -k $s && . $s
+    done
+    unset s
 fi
 unset bash bmajor bminor
 
