50ba291711e951c39f1277457551504985444391
Maxim Fedorenko <varlllog@gmail.com>
2017-09-14 15:26:09 +0300

    [mac] Update configs from macbook

--- mac/etc/profile.d/bash_completion.sh
+++ mac/etc/profile.d/bash_completion.sh
@@ -4,10 +4,11 @@ if [ -n "$BASH_VERSION" -a -n "$PS1" -a -z "$BASH_COMPLETION_COMPAT_DIR" ]; then
 # Check for recent enough version of bash.
 bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
 if [ $bmajor -gt 4 ] || [ $bmajor -eq 4 -a $bminor -ge 1 ]; then
-    for s in /usr/local/etc/bash_completion.d/* ; do
-        test -r $s -a ! -k $s && . $s
-    done
-    unset s
+
+    if [ -f $(brew --prefix)/etc/bash_completion ]; then
+        . $(brew --prefix)/etc/bash_completion
+    fi
+
 fi
 unset bash bmajor bminor
 
