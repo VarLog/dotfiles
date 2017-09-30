2495cb2b16af9fd4eabbea0647d57219b54ebe37
Maxim Fedorenko <varlllog@gmail.com>
2017-09-30 13:41:09 +0300

    Add mongodb rc-file

---
+++ general/home/mongorc.js
@@ -0,0 +1,16 @@
+EDITOR="vim"
+
+prompt = function() {
+    if (typeof db == 'undefined') {
+        return '(nodb)> ';
+    }
+
+    // Check the last db operation
+    try {
+        db.runCommand({getLastError:1});
+    }
+    catch (e) {
+        print(e);
+    }
+    return db+"> ";
+};
